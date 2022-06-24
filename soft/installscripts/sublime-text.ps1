a43421e797b9e02d69eecf51c96fa6d29e5919f8
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-24 14:59:33 +0300

    Create sublime-text.ps1

---
+++ soft/installscripts/sublime-text.ps1
@@ -0,0 +1,9 @@
+mkdir c:\temp 
+# downlowd Sublime Text
+$source = 'https://www.sublimetext.com/download_thanks?target=win-x64'
+# Destination to save the file
+$destination = 'c:\temp\sublimetext.exe'
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destination
+cd c:\temp\
+start-process -FilePath 'c:\temp\sublimetext.exe' -ArgumentList '/VERYSILENT'
