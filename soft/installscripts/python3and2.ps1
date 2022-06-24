7459c51344fd879ebf6625cb5690d593c70d9afe
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-24 13:57:37 +0300

    Update and rename python3.ps1 to python3and2.ps1

--- soft/installscripts/python3and2.ps1
+++ soft/installscripts/python3and2.ps1
@@ -143,3 +143,17 @@ Add-EnvPath 'C:\Python\'
 
 # Install a library using Pip
 python -m pip install numpy
+
+# PowerShell to install Python, Boto, and AWSCLI
+# Check for latest Python version and update link in following line.
+Invoke-WebRequest -Outfile $env:userprofile\python.msi https://www.python.org/ftp/python/2.7.9/python-2.7.9.amd64.msi
+msiexec.exe /i  $env:userprofile\python.msi /quiet
+set PATH "%PATH%;C:\Python27;C:\Python27\Scripts"
+setx PATH "%PATH%;C:\Python27;C:\Python27\Scripts"
+Invoke-WebRequest -Outfile  $env:userprofile\pip.py https://bootstrap.pypa.io/get-pip.py
+sleep 30
+C:\Python27\python.exe  $env:userprofile\pip.py
+C:\Python27\Scripts\pip.exe install boto
+C:\Python27\Scripts\pip.exe install awscli
+del $env:userprofile\pip.py
+del $env:userprofile\python.msi
