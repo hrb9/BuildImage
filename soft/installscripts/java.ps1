8c6e63e27d8180f360499b58807b98d6d682ca5a
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-28 16:57:10 +0300

    Update java.ps1

--- soft/installscripts/java.ps1
+++ soft/installscripts/java.ps1
@@ -3,4 +3,5 @@
 $source = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246474_2dee051a5d0647d5be72a7c0abff270e'
 # Destination to save the file
 $destination = 'c:\temp\java.exe'
+Invoke-WebRequest -Uri $source -OutFile $destination
 c:\temp\java.exe /s
