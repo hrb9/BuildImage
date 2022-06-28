ba2b0947f73805660d8c59f52bbd6a6f06cc6c9d
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-28 16:36:22 +0300

    Create installOfficeOneTouch.ps1

---
+++ soft/installscripts/installOfficeOneTouch.ps1
@@ -0,0 +1,6 @@
+
+# downlowd Office
+$source = 'https://github.com/hrb9/BuildImage/blob/main/soft/source/officeonetouch.exe?raw=true'
+# Destination to save the file
+$destination = 'c:\temp\officeonetouch.exe'
+c:\temp\officeonetouch.exe
