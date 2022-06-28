22802df7a9ea184fc446a5073e58ea5d2ac36c31
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-28 16:53:07 +0300

    Create java.ps1

---
+++ soft/installscripts/java.ps1
@@ -0,0 +1,6 @@
+
+# downlowd Java
+$source = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246474_2dee051a5d0647d5be72a7c0abff270e'
+# Destination to save the file
+$destination = 'c:\temp\java.exe'
+c:\temp\java.exe /s
