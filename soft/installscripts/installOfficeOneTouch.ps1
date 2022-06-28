24d46fc35201f83ef3e3b07de53e9e32c7f82733
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-28 16:56:52 +0300

    Update installOfficeOneTouch.ps1

--- soft/installscripts/installOfficeOneTouch.ps1
+++ soft/installscripts/installOfficeOneTouch.ps1
@@ -1,6 +1,8 @@
 
 # downlowd Office
 $source = 'https://github.com/hrb9/BuildImage/blob/main/soft/source/officeonetouch.exe?raw=true'
+
 # Destination to save the file
 $destination = 'c:\temp\officeonetouch.exe'
+Invoke-WebRequest -Uri $source -OutFile $destination
 c:\temp\officeonetouch.exe
