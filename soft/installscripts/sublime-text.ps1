851180b15bf5ecb7da8058fc8c14cd50dc7ad382
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-29 17:12:16 +0300

    Update sublime-text.ps1

--- soft/installscripts/sublime-text.ps1
+++ soft/installscripts/sublime-text.ps1
@@ -1,6 +1,6 @@
 mkdir c:\temp 
 # downlowd Sublime Text
-$source = 'https://www.sublimetext.com/download_thanks?target=win-x64'
+$source = 'https://www.sublimetext.com/download_thanks?target=win-x64#direct-downloads'
 # Destination to save the file
 $destination = 'c:\temp\sublimetext.exe'
 #Download the file
