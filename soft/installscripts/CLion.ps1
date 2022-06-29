e2bf8b279539aec4324ec521f0766f0bf9f9aeaf
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-06-29 17:09:01 +0300

    Update CLion.ps1

--- soft/installscripts/CLion.ps1
+++ soft/installscripts/CLion.ps1
@@ -1,6 +1,6 @@
 mkdir c:\temp 
 # downlowd CLion
-$source = 'https://www.jetbrains.com/clion/download/download-thanks.html?platform=windows'
+$source = 'https://download.jetbrains.com/cpp/CLion-2022.1.3.exe?_gl=1*1c7v9b2*_ga*NDEwNDMzMTEzLjE2NTY1MTE1NzU.*_ga_9J976DJZ68*MTY1NjUxMTU3NC4xLjEuMTY1NjUxMTY3NS4w&_ga=2.216001054.884450988.1656511575-410433113.1656511575'
 # Destination to save the file
 $destination = 'c:\temp\clion.exe'
 #Download the file
