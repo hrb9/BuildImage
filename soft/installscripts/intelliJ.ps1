dd41e395a17d2338d218d059402aacf72ad614fe
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-07-04 19:31:28 +0300

    Update intelliJ.ps1

--- soft/installscripts/intelliJ.ps1
+++ soft/installscripts/intelliJ.ps1
@@ -10,5 +10,5 @@ $sourceconf = 'https://raw.githubusercontent.com/hrb9/BuildImage/main/soft/insta
 $destinationconf = 'c:\temp\silentintellij.config'
 #Download the file
 Invoke-WebRequest -Uri $sourceconf -OutFile $destinationconf
-#cd c:\temp\
-#c:\temp\intellij.exe /S /CONFIG=c:\temp\silentCLion.config /D=c:\IDE\intellij
+cd c:\temp\
+c:\temp\intellij.exe /S /CONFIG=c:\temp\silentCLion.config /D=c:\IDE\intellij
