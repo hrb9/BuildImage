5cecdbd1750c73dc104bee527b09a865458aa2a7
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-07-04 17:50:37 +0300

    Update vscode.ps1

--- soft/installscripts/vscode.ps1
+++ soft/installscripts/vscode.ps1
@@ -5,5 +5,5 @@ $source = 'https://raw.githubusercontent.com/hrb9/BuildImage/main/soft/source/vs
 $destination = 'c:\temp\vscodeinstall.ps1'
 #Download the file
 Invoke-WebRequest -Uri $source -OutFile $destination
-cd c:\temp\
-c:\temp\vscodeinstall.ps1 -Architecture '64-bit'  
+#cd c:\temp\
+#c:\temp\vscodeinstall.ps1 -Architecture '64-bit'  
