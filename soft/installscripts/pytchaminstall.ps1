50e8ee50a5f6fb828922311c1a362be24b0f2aaa
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-07-05 04:39:46 +0300

    Update pytchaminstall.ps1

--- soft/installscripts/pytchaminstall.ps1
+++ soft/installscripts/pytchaminstall.ps1
@@ -10,5 +10,5 @@ $sourceconf = 'https://raw.githubusercontent.com/hrb9/BuildImage/main/soft/insta
 $destinationconf = 'c:\temp\silent.config'
 #Download the file
 Invoke-WebRequest -Uri $sourceconf -OutFile $destinationconf
-cd c:\temp\
-c:\temp\pytcham.exe /S /CONFIG=c:\temp\silent.config /D=c:\IDE\PyCharm
+#cd c:\temp\
+#c:\temp\pytcham.exe /S /CONFIG=c:\temp\silent.config /D=c:\IDE\PyCharm
