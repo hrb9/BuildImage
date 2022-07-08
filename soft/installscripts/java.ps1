c9c6ee23ffed651035b5dd3e09311f1d092d7ae8
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-07-08 14:39:25 +0000

    Update java.ps1

--- soft/installscripts/java.ps1
+++ soft/installscripts/java.ps1
@@ -3,9 +3,9 @@
 $source = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246474_2dee051a5d0647d5be72a7c0abff270e'
 # Destination to save the file
 $destination = 'c:\temp\java.exe'
-Invoke-WebRequest -Uri $source -OutFile $destination
+$wc = New-Object net.webclient
+$wc.Downloadfile($source, $destination)
 #Execute-Process 'c:\temp\java.exe' -Arguments '/s INSTALL_SILENT=1 REBOOT=0 /L c:\temp\jre-8u45-windows-x64.log'
-
 $pathvargs = {c:\temp\java.exe INSTALL_SILENT=1 }
 Invoke-Command -ScriptBlock $pathvargs
 
