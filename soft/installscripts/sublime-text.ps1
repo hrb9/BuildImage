dfc1080225d53fdfff1cb5d6ca220077e35f1734
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-07-08 14:48:30 +0000

    Update sublime-text.ps1

--- soft/installscripts/sublime-text.ps1
+++ soft/installscripts/sublime-text.ps1
@@ -4,7 +4,8 @@ $source = 'https://download.sublimetext.com/sublime_text_build_4126_x64_setup.ex
 # Destination to save the file
 $destination = 'c:\temp\sublimetext.exe'
 #Download the file
-Invoke-WebRequest -Uri $source -OutFile $destination
+$wc = New-Object net.webclient
+$wc.Downloadfile($source, $destination)
 
 $pathvargs = {c:\temp\sublimetext.exe /SP- /VERYSILENT /NORESTART }
 Invoke-Command -ScriptBlock $pathvargs
