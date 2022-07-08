641ed727db5cf67fb523ef33d9d309b8f59a3203
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-07-08 15:17:08 +0300

    Update java.ps1

--- soft/installscripts/java.ps1
+++ soft/installscripts/java.ps1
@@ -6,6 +6,6 @@ $destination = 'c:\temp\java.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
 #Execute-Process 'c:\temp\java.exe' -Arguments '/s INSTALL_SILENT=1 REBOOT=0 /L c:\temp\jre-8u45-windows-x64.log'
 
-#$pathvargs = {c:\temp\java.exe INSTALL_SILENT=1 }
-#Invoke-Command -ScriptBlock $pathvargs
+$pathvargs = {c:\temp\java.exe INSTALL_SILENT=1 }
+Invoke-Command -ScriptBlock $pathvargs
 
