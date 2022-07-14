da90c6402a78a4817ed72beb994e34bfcfba25a7
hrb9 <31981104+hrb9@users.noreply.github.com>
2022-07-14 13:01:40 +0300

    ADD Sublime Text

--- soft/installAutoSomeAppDev.ps1
+++ soft/installAutoSomeAppDev.ps1
@@ -260,3 +260,15 @@ $destination = 'c:\temp\vscodeinstall.ps1'
 Invoke-WebRequest -Uri $source -OutFile $destination
 #cd c:\temp\
 c:\temp\vscodeinstall.ps1 -Architecture '64-bit'  
+
+#Sublime Text#
+# downlowd Sublime Text
+$source = 'https://download.sublimetext.com/sublime_text_build_4126_x64_setup.exe'
+# Destination to save the file
+$destination = 'c:\temp\sublimetext.exe'
+#Download the file
+$wc = New-Object net.webclient
+$wc.Downloadfile($source, $destination)
+
+$pathvargs = {c:\temp\sublimetext.exe /SP- /VERYSILENT /NORESTART }
+Invoke-Command -ScriptBlock $pathvargs
